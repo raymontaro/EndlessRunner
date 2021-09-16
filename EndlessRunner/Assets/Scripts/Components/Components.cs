a4a008107264b835eb6847da926be82e239c0388
raymontaro <mega.ray.mon93@gmail.com>
2021-09-16 10:54:02 +0700

    Initial commit

---
+++ EndlessRunner/Assets/Scripts/Components/Components.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+public struct Spawn : IBufferElementData
+{
+    public Entity entity;
+}
+
+public struct UIElement : IBufferElementData
+{
+    public Entity entity;
+}
---
+++ EndlessRunner/Assets/Scripts/Systems/LevelChunkSpawnSystem.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+[UpdateAfter(typeof(LevelChunkMoveSystem))]
+public class LevelChunkSpawnSystem : ComponentSystem
+{
+    public static LevelChunkSpawnSystem Instance;
+
+    Random random;
+    bool init = false;
+
+    protected override void OnStartRunning()
+    {
+        Instance = this;
+    }
+
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<Spawn>();
+
+        random = new Random(314159);                
+    }
+
+    protected override void OnUpdate()
+    {
+        var spawnEntity = GetSingletonEntity<Spawn>();
+
+
+        int initPos = -10;
+
+        if (!init)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var spawnBuffer = EntityManager.GetBuffer<Spawn>(spawnEntity);
+                var spawnedEntity = EntityManager.Instantiate(spawnBuffer[0].entity);
+                EntityManager.SetComponentData(spawnedEntity, new Translation { Value = new float3(0, 0, initPos) });
+                initPos += 10;
+            }
+
+            init = true;
+            return;
+        }
+        
+    }
+
+    public void SpawnLevelChunk()
+    {
+        var spawnEntity = GetSingletonEntity<Spawn>();
+        var spawnBuffer = EntityManager.GetBuffer<Spawn>(spawnEntity);
+        var spawnedEntity = EntityManager.Instantiate(spawnBuffer[random.NextInt(spawnBuffer.Length)].entity);
+        
+        EntityManager.SetComponentData(spawnedEntity, new Translation { Value = new float3(0, 0, 30) });        
+    }
+}
